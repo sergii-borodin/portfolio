739259d9f1cf3ee9cbfe4d9dd917599b208cea78
BoroDinS <sergey.borodin.lg@gmail.com>
2022-09-30 12:29:01 +0200

    Updates

---
+++ precache-manifest.204e4f4e1a02ca19d24f89d479226236.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7d925678563913d69c3",
+    "url": "/static/css/main.d02542e5.chunk.css"
+  },
+  {
+    "revision": "e7d925678563913d69c3",
+    "url": "/static/js/main.d123502d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2bd6ab24e327e0d9c631",
+    "url": "/static/js/2.da360e16.chunk.js"
+  },
+  {
+    "revision": "d365ea4ff69036ddf637c0c550bc95c7",
+    "url": "/static/media/hero-image1.d365ea4f.jpeg"
+  },
+  {
+    "revision": "64575681499d08b0f7df52eb2b6389aa",
+    "url": "/static/media/CV.64575681.pdf"
+  },
+  {
+    "revision": "162c6b32546c48562d407d8f17065c6e",
+    "url": "/static/media/kaospilot-toolbox.162c6b32.jpg"
+  },
+  {
+    "revision": "9ece321e45320d0904e35233bcfe89ea",
+    "url": "/static/media/meal-sharing.9ece321e.jpg"
+  },
+  {
+    "revision": "a80a90015e4a63d3cf6a71875f27937e",
+    "url": "/static/media/html.a80a9001.png"
+  },
+  {
+    "revision": "cf71a98c6bc707f2d070388c7afcb0c9",
+    "url": "/static/media/css.cf71a98c.png"
+  },
+  {
+    "revision": "68777e9738cf44a28fe3db67e269f224",
+    "url": "/static/media/javascript.68777e97.png"
+  },
+  {
+    "revision": "1b28808441932b7571626b4edcc307fe",
+    "url": "/static/media/react.1b288084.png"
+  },
+  {
+    "revision": "59863c6180caa67a7d0c00e13727a828",
+    "url": "/static/media/git.59863c61.png"
+  },
+  {
+    "revision": "3938e0e2787cbf423ab5b52f29792bdf",
+    "url": "/static/media/sql.3938e0e2.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/static/media/node.55e83371.png"
+  },
+  {
+    "revision": "09ae7b8dfd44ff584d07d14419ea2feb",
+    "url": "/static/media/hyf.09ae7b8d.png"
+  },
+  {
+    "revision": "a82b1bd80011ec3d891476761c9e2e00",
+    "url": "/index.html"
+  }
+];
