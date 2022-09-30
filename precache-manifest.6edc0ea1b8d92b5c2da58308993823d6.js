7d85276cae35a958be41fedceafca0e04d5cecf6
BoroDinS <sergey.borodin.lg@gmail.com>
2022-09-30 17:27:21 +0200

    Updates

---
+++ precache-manifest.6edc0ea1b8d92b5c2da58308993823d6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7d925678563913d69c3",
+    "url": "/portfolio/static/css/main.d02542e5.chunk.css"
+  },
+  {
+    "revision": "e7d925678563913d69c3",
+    "url": "/portfolio/static/js/main.d123502d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "2bd6ab24e327e0d9c631",
+    "url": "/portfolio/static/js/2.da360e16.chunk.js"
+  },
+  {
+    "revision": "d365ea4ff69036ddf637c0c550bc95c7",
+    "url": "/portfolio/static/media/hero-image1.d365ea4f.jpeg"
+  },
+  {
+    "revision": "64575681499d08b0f7df52eb2b6389aa",
+    "url": "/portfolio/static/media/CV.64575681.pdf"
+  },
+  {
+    "revision": "162c6b32546c48562d407d8f17065c6e",
+    "url": "/portfolio/static/media/kaospilot-toolbox.162c6b32.jpg"
+  },
+  {
+    "revision": "9ece321e45320d0904e35233bcfe89ea",
+    "url": "/portfolio/static/media/meal-sharing.9ece321e.jpg"
+  },
+  {
+    "revision": "a80a90015e4a63d3cf6a71875f27937e",
+    "url": "/portfolio/static/media/html.a80a9001.png"
+  },
+  {
+    "revision": "cf71a98c6bc707f2d070388c7afcb0c9",
+    "url": "/portfolio/static/media/css.cf71a98c.png"
+  },
+  {
+    "revision": "68777e9738cf44a28fe3db67e269f224",
+    "url": "/portfolio/static/media/javascript.68777e97.png"
+  },
+  {
+    "revision": "1b28808441932b7571626b4edcc307fe",
+    "url": "/portfolio/static/media/react.1b288084.png"
+  },
+  {
+    "revision": "59863c6180caa67a7d0c00e13727a828",
+    "url": "/portfolio/static/media/git.59863c61.png"
+  },
+  {
+    "revision": "3938e0e2787cbf423ab5b52f29792bdf",
+    "url": "/portfolio/static/media/sql.3938e0e2.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/portfolio/static/media/node.55e83371.png"
+  },
+  {
+    "revision": "09ae7b8dfd44ff584d07d14419ea2feb",
+    "url": "/portfolio/static/media/hyf.09ae7b8d.png"
+  },
+  {
+    "revision": "3cc5c4a748f91f1d3154f26b9d251ada",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.204e4f4e1a02ca19d24f89d479226236.js"
+  "/portfolio/precache-manifest.6edc0ea1b8d92b5c2da58308993823d6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
