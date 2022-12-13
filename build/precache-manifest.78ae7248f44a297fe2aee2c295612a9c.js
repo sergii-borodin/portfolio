7c1cde62a72284e42f9d5389af311c00ff6f8f66
BoroDinS <sergey.borodin.lg@gmail.com>
2022-12-13 14:43:34 +0100

    new build

---
+++ build/precache-manifest.78ae7248f44a297fe2aee2c295612a9c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "dee483b9c2ca3eaa3d53",
+    "url": "/portfolio/static/css/main.0364a597.chunk.css"
+  },
+  {
+    "revision": "dee483b9c2ca3eaa3d53",
+    "url": "/portfolio/static/js/main.507e7eed.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "597e3c337b1beb56be22",
+    "url": "/portfolio/static/js/2.45992847.chunk.js"
+  },
+  {
+    "revision": "05c4f563c813f8b18a40c999f29bcf5e",
+    "url": "/portfolio/static/media/hero-image-light.05c4f563.jpeg"
+  },
+  {
+    "revision": "47a474f66668ce1b057afed57ec2bf17",
+    "url": "/portfolio/static/media/hero-image-dark.47a474f6.jpeg"
+  },
+  {
+    "revision": "3697d6e1760041dea292dcc5164b0e1d",
+    "url": "/portfolio/static/media/CV.3697d6e1.pdf"
+  },
+  {
+    "revision": "162c6b32546c48562d407d8f17065c6e",
+    "url": "/portfolio/static/media/kaospilot-toolbox.162c6b32.jpg"
+  },
+  {
+    "revision": "9ece321e45320d0904e35233bcfe89ea",
+    "url": "/portfolio/static/media/meal-sharing.9ece321e.jpg"
+  },
+  {
+    "revision": "cfbf71dfa72f3acbc756b0a46ee11852",
+    "url": "/portfolio/static/media/ice-cream-shop.cfbf71df.jpg"
+  },
+  {
+    "revision": "a80a90015e4a63d3cf6a71875f27937e",
+    "url": "/portfolio/static/media/html.a80a9001.png"
+  },
+  {
+    "revision": "cf71a98c6bc707f2d070388c7afcb0c9",
+    "url": "/portfolio/static/media/css.cf71a98c.png"
+  },
+  {
+    "revision": "68777e9738cf44a28fe3db67e269f224",
+    "url": "/portfolio/static/media/javascript.68777e97.png"
+  },
+  {
+    "revision": "1b28808441932b7571626b4edcc307fe",
+    "url": "/portfolio/static/media/react.1b288084.png"
+  },
+  {
+    "revision": "59863c6180caa67a7d0c00e13727a828",
+    "url": "/portfolio/static/media/git.59863c61.png"
+  },
+  {
+    "revision": "3938e0e2787cbf423ab5b52f29792bdf",
+    "url": "/portfolio/static/media/sql.3938e0e2.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/portfolio/static/media/node.55e83371.png"
+  },
+  {
+    "revision": "09ae7b8dfd44ff584d07d14419ea2feb",
+    "url": "/portfolio/static/media/hyf.09ae7b8d.png"
+  },
+  {
+    "revision": "dcb794a9cac1fdbc7387a7f825e5526b",
+    "url": "/portfolio/index.html"
+  }
+];
