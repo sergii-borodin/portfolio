1841dad5a775d2bfb55865fb5cc8a5f87d6c59fb
BoroDinS <sergey.borodin.lg@gmail.com>
2023-07-11 20:31:18 +0200

    add typescript in skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-import html from '../../assets/skills/html.png'
-import css from '../../assets/skills/css.png'
-import javascript from '../../assets/skills/javascript.png'
-import reactImage from '../../assets/skills/react.png'
-import github from '../../assets/skills/git.png'
-import sql from '../../assets/skills/sql.png'
-import node from '../../assets/skills/node.png'
+import html from '../../assets/skills/html.png';
+import css from '../../assets/skills/css.png';
+import javascript from '../../assets/skills/javascript.png';
+import reactImage from '../../assets/skills/react.png';
+import github from '../../assets/skills/git.png';
+import sql from '../../assets/skills/sql.png';
+import node from '../../assets/skills/node.png';
+import ts from '../../assets/skills/ts.png';
 
-import './skills.css'
+import './skills.css';
 
 export const Skills = () => {
   const techs = [
@@ -47,18 +48,23 @@ export const Skills = () => {
       src: node,
       title: 'NodeJS',
     },
-  ]
+    {
+      id: 8,
+      src: ts,
+      title: 'TypeScript',
+    },
+  ];
   return (
-    <section name='skills' className='section container experience-section'>
-      <h2 className='section-title'>My stack and tools</h2>
-      <ul className='tools-grid-area'>
+    <section name="skills" className="section container experience-section">
+      <h2 className="section-title">My stack and tools</h2>
+      <ul className="tools-grid-area">
         {techs.map(({ id, src, title }) => (
-          <li className='tool-card box-shadow' key={id}>
-            <img className='tool-image' src={src} alt='' />
+          <li className="tool-card box-shadow" key={id}>
+            <img className="tool-image" src={src} alt="" />
             <p>{title}</p>
           </li>
         ))}
       </ul>
     </section>
-  )
-}
+  );
+};
