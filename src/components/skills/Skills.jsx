474299188239f6db4e33fddedaf48a4351cdfa58
Sergii Borodin <sergii.borodin@yahoo.com>
2026-07-18 23:30:19 +0200

    Add new SVG assets for Expo, MongoDB, Next.js, and Tailwind; update Skills component to include these technologies and adjust CSS for improved layout and responsiveness.

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -8,6 +8,10 @@ import github from '../../assets/skills/git.png';
 import sql from '../../assets/skills/sql.png';
 import node from '../../assets/skills/node.png';
 import ts from '../../assets/skills/ts.png';
+import nextjs from '../../assets/skills/nextjs.svg';
+import mongodb from '../../assets/skills/mongodb.svg';
+import tailwind from '../../assets/skills/tailwind.svg';
+import expo from '../../assets/skills/expo.svg';
 
 import './skills.css';
 
@@ -30,28 +34,48 @@ export const Skills = () => {
     },
     {
       id: 4,
+      src: ts,
+      title: 'TypeScript',
+    },
+    {
+      id: 5,
       src: reactImage,
       title: 'React/React Native',
     },
     {
-      id: 5,
-      src: github,
-      title: 'Git',
-    },
-    {
       id: 6,
-      src: sql,
-      title: 'SQL',
+      src: nextjs,
+      title: 'Next.js',
     },
     {
       id: 7,
+      src: tailwind,
+      title: 'Tailwind',
+    },
+    {
+      id: 8,
       src: node,
       title: 'NodeJS',
     },
     {
-      id: 8,
-      src: ts,
-      title: 'TypeScript',
+      id: 9,
+      src: mongodb,
+      title: 'MongoDB',
+    },
+    {
+      id: 10,
+      src: sql,
+      title: 'SQL',
+    },
+    {
+      id: 11,
+      src: expo,
+      title: 'Expo',
+    },
+    {
+      id: 12,
+      src: github,
+      title: 'Git',
     },
   ];
   return (
